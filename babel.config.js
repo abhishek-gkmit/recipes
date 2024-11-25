94d9b41a42d8133a774ef192da2ec925f541e7a0
Abhishek <abhishek@gkmit.co>
2024-11-25 16:14:47 +0530

    feat: add project setup

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,26 @@
 module.exports = {
   presets: ['module:@react-native/babel-preset'],
+  plugins: [
+    [
+      'module-resolver',
+      {
+        alias: {
+          '@assets': './src/assets',
+          '@constants': './src/constants',
+          '@hooks': './src/hooks',
+          '@screens': './src/screens',
+          '@utility': './src/utility',
+          '@components': './src/components',
+          '@navigation': './src/navigation',
+          '@network': './src/network',
+          '@storage': './src/storage',
+          '@theme': './src/theme',
+          '@config': './src/config',
+          '@contexts': './src/contexts',
+        },
+      },
+    ],
+    ['module:react-native-dotenv'],
+    'react-native-reanimated/plugin',
+  ],
 };
