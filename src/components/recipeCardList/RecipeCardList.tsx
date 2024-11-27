2251804b8332fb0bc80b873e23a0f489d4063594
Abhishek <abhishek@gkmit.co>
2024-11-27 11:35:47 +0530

    fix: fix recipe card list - add loader and title in `RecipeCardList`

--- src/components/recipeCardList/RecipeCardList.tsx
+++ src/components/recipeCardList/RecipeCardList.tsx
@@ -2,13 +2,17 @@ import { View, FlatList, StyleSheet } from 'react-native';
 
 import TextBlock from '@components/customText';
 import RecipeCard from '@components/recipeCard';
+import Loader from '@components/customLoader';
+import EmptyListIndicator from '@components/emptyListIndicator';
 import useStyles from '@hooks/useStyles';
 
 import recipeCardListStyles from './styles';
+import { useMemo } from 'react';
 
 function RecipeCardList({
   title,
   data,
+  loading,
   titleStyles,
   listContainerStyles,
   listStyles,
@@ -17,30 +21,42 @@ function RecipeCardList({
 }: RecipeCardListProps) {
   const styles = useStyles(recipeCardListStyles);
 
+  const textBlock = useMemo(() => {
+    return title ? (
+      <TextBlock style={StyleSheet.compose(styles.listTitle, titleStyles)}>
+        {title}
+      </TextBlock>
+    ) : null;
+  }, [styles, titleStyles, title]);
+
+  const cardList = loading ? (
+    <Loader />
+  ) : (
+    <FlatList
+      data={data}
+      keyExtractor={item => item.id}
+      renderItem={({ item }) => <RecipeCard {...item} />}
+      horizontal
+      style={listStyles}
+      contentContainerStyle={StyleSheet.compose(
+        styles.recipeCardListContent,
+        listContentContainerStyles,
+      )}
+      showsHorizontalScrollIndicator={false}
+      initialNumToRender={3}
+      ListEmptyComponent={<EmptyListIndicator title="recipes" />}
+      {...props}
+    />
+  );
+
   return (
     <View
       style={StyleSheet.compose(
         styles.recipeCardListContainer,
         listContainerStyles,
       )}>
-      <TextBlock style={StyleSheet.compose(styles.listTitle, titleStyles)}>
-        {title}
-      </TextBlock>
-
-      <FlatList
-        data={data}
-        keyExtractor={item => item.id}
-        renderItem={({ item }) => <RecipeCard {...item} />}
-        horizontal
-        style={listStyles}
-        contentContainerStyle={StyleSheet.compose(
-          styles.recipeCardListContent,
-          listContentContainerStyles,
-        )}
-        showsHorizontalScrollIndicator={false}
-        initialNumToRender={3}
-        {...props}
-      />
+      {textBlock}
+      {cardList}
     </View>
   );
 }
